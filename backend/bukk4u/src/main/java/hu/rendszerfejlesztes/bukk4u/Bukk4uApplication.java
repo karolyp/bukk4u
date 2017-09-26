994e223e0d7ab51696ce60d64116d2fcee4bbd8e
Pákozdi Károly <karoly.pakozdi@frontendart.com>
2017-09-26 17:56:17 +0200

    Adding WAR packaging

--- backend/bukk4u/src/main/java/hu/rendszerfejlesztes/bukk4u/Bukk4uApplication.java
+++ backend/bukk4u/src/main/java/hu/rendszerfejlesztes/bukk4u/Bukk4uApplication.java
@@ -2,10 +2,17 @@ package hu.rendszerfejlesztes.bukk4u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Bukk4uApplication  {
+public class Bukk4uApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Bukk4uApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Bukk4uApplication.class, args);
 	}
