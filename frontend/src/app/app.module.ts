3dbae73ae1aaaa8bc584f82d2bd40675114885ea
Pákozdi Károly <karoly.pakozdi@frontendart.com>
2017-10-26 23:30:32 +0200

    Adding new gányolások

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatToolbarModule,
-  MatTooltipModule} from '@angular/material';
+  MatCheckboxModule, MatMenuModule, MatIconModule, MatButtonModule, MatFormFieldModule,
+  MatInputModule, MatToolbarModule, MatDialogModule
+} from '@angular/material';
 
-import { AppComponent } from './app.component';
+import {NgbModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+const appRoutes: Routes = [
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    MatDialogModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
-    MatToolbarModule,
     MatButtonModule,
-    MatTooltipModule
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    NgbModule.forRoot(),
+    NgbAlertModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [LoginComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
