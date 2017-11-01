6b2090f3e55f9b8c0f3b90f76f3855ef897cdf95
zoltanvi <viczezoltan@gmail.com>
2017-11-01 14:33:27 +0100

    Created book component in list view

--- frontend/src/app/book-list/book-list.component.ts
+++ frontend/src/app/book-list/book-list.component.ts
@@ -15,7 +15,24 @@ export class BookListComponent implements OnInit {
       'Ray Seyfarth',
       6402,
       'CreateSpace Independent Publishing Platform',
+      true),
+    new Book(9781543138849,
+      'Introduction to 64 Bit Windows Assembly Language Programming: Fourth Edition',
+      'This book introduces programmers to 64 bit Intel assembly language using the Microsoft Windows operating system. The book also discusses how to use the free integrated development environment, ebe, designed by the author specifically to meet the needs of assembly language programmers. Ebe is a C++ program which uses the Qt library to implement a GUI environment consisting of a source window, a data window, a register window, a floating point register window, a backtrace window, a console window, a terminal window, a project window and a pair of teaching tools called the "Toy Box" and the "Bit Bucket".',
+      'Education',
+      'Ray Seyfarth',
+      6402,
+      'CreateSpace Independent Publishing Platform',
+      true),
+    new Book(9781543138849,
+      'Introduction to 64 Bit Windows Assembly Language Programming: Fourth Edition',
+      'This book introduces programmers to 64 bit Intel assembly language using the Microsoft Windows operating system. The book also discusses how to use the free integrated development environment, ebe, designed by the author specifically to meet the needs of assembly language programmers. Ebe is a C++ program which uses the Qt library to implement a GUI environment consisting of a source window, a data window, a register window, a floating point register window, a backtrace window, a console window, a terminal window, a project window and a pair of teaching tools called the "Toy Box" and the "Bit Bucket".',
+      'Education',
+      'Ray Seyfarth',
+      6402,
+      'CreateSpace Independent Publishing Platform',
       true)
+
   ];
   constructor() { }
 
--- frontend/src/app/book-list/book/book.component.ts
+++ frontend/src/app/book-list/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'book',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+
+ @Input() book: {isbn: number, title: string, details: string,
+    category: string, author: string, price: number,
+    publisher: string, onStock: boolean};
 
   constructor() { }
 
