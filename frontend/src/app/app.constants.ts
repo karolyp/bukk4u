879dd376266ea43869ec40b723c783b6a17ebb22
Varju Norbert <norbivar96@gmail.com>
2017-11-30 14:28:58 +0100

    Fixed service call -> now it will send to Azure server

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
-  public static API : string  = "http://localhost:8080/api";
+  public static API : string  = "http://http://bukk4u.azurewebsites.net:8080/api";
   public static TOKEN_EXPIRATION: number = 180; // in minutes
   // public static API : string  = "http://rf-1.tk/bookshop-backend/api";
 }
